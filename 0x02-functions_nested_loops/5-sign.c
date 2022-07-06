4965fc93358fb328cf47b828337ed7f9f2b2c834
vicron <vicron@example.com>
2022-07-06 15:20:40 -0700

    printing the signs fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_sign -> prints the sign of a number
+ * print_sign - prints the sign of a number
  *
  * @n: it is a character for memory location
  *
- * Return: 1, 0 -1 
+ * Return: returns 1, 0 and -1 depending on the condition given.
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
 _putchar('+');
-return(1);
 }
 else if (n == 0)
 {
 _putchar('0');
 }
-else
+else if (n < 0)
 {
 _putchar('-');
-return (-1);
 }
+return(1 || 0 || -1);
+return (0);
 }
