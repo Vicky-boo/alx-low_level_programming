c333ff097e4c122e5ac415d5a1c49c5e3189c48e
vicron <vicron@example.com>
2022-07-06 15:41:52 -0700

    printing 5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  *
- * @n: it is a character for memory location
+ * @n: argument passed
  *
- * Return: returns 1, 0 and -1 depending on the condition given.
+ * Return:  0, 1 and -1
  */
 int print_sign(int n)
 {
@@ -21,6 +21,6 @@ else if (n < 0)
 {
 _putchar('-');
 }
-return(1 || 0 || -1);
+return (1 || 0 || -1);
 return (0);
 }
