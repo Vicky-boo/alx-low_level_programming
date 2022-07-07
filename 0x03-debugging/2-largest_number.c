c7b9dddc2fd0748a9c62694132c75683535a2f0e
vicron <vicron@example.com>
2022-07-07 16:07:31 -0700

    new fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,19 @@ int largest_number(int a, int b, int c)
 int largest;
 if (a > b && a > c)
 {
-  largest = a;
+largest = a;
 }
 else if (b > a && b > c)
 {
-  largest = b;
+largest = b;
 }
 else if (c > b)
 {
-  largest = c;
+largest = c;
 }
 else
 {
-	largest = b;
+largest = b;
 }
 
 return (largest);
