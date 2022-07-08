d7c14a751c7c2d8f38cfa3fcd7c5abfb08147537
vicron <vicron@example.com>
2022-07-08 15:16:52 -0700

    interviee test alao fiz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz-buzz test
+ *
+ * Return: returns value
+ */
+int main(void)
+{
+int num;
+for (num = 1; num <= 100; num++)
+{
+if (num % 3 == 0 && num % 5 != 0)
+  printf("Fizz ");
+else if (num % 5 == 0 && num % 3 != 0)
+  printf("Buzz ");
+else if (num == 100)
+  printf("Buzz");
+else if (num % 3 == 0 && num % 5 == 0)
+  printf("FizzBuzz ");
+else
+  printf("%d ", num);
+}
+printf("\n");
+return (0);
+}
