ed36edac1abb661b8a58df786e144c931651035f
vicron <vicron@example.com>
2022-07-08 14:49:51 -0700

    printimg number from zero to nine

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints digits from 0 to 9 except 2 and 4
+*/
+void print_most_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+if (a != 2 && a != 4)
+_putchar((a % 10) + '0');
+}
+_putchar('\n');
+}
