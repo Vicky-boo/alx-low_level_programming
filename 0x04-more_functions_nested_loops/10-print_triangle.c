fcc75e8194a588a43b92806cfceae8fadaf2d2b1
vicron <vicron@example.com>
2022-07-08 15:40:48 -0700

    function thay print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints the shape of a triangle
+ * @size: its the size of the triangle
+ */
+void print_triangle(int size)
+{
+int a, b, c;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+c = (size - a) - 1;
+if (b < c)
+_putchar(' ');
+else
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
