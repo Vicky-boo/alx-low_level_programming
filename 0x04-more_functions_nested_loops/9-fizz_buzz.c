1d0fc4f3216588822531d6dc0d4be7ea9c31e2cf
vicron <vicron@example.com>
2022-07-08 15:21:55 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints fizz-buzz test
- *
- * Return: returns value
- */
+* main - prints fizz-buzz test
+*
+* Return: returns value
+*/
 int main(void)
 {
 int num;
 for (num = 1; num <= 100; num++)
 {
 if (num % 3 == 0 && num % 5 != 0)
-  printf("Fizz ");
+printf("Fizz ");
 else if (num % 5 == 0 && num % 3 != 0)
-  printf("Buzz ");
+printf("Buzz ");
 else if (num == 100)
-  printf("Buzz");
+printf("Buzz");
 else if (num % 3 == 0 && num % 5 == 0)
-  printf("FizzBuzz ");
+printf("FizzBuzz ");
 else
-  printf("%d ", num);
+printf("%d ", num);
 }
 printf("\n");
 return (0);
