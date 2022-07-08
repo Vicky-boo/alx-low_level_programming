504728ee740461ff66b142d7e2a9f7621e9b6f58
vicron <vicron@example.com>
2022-07-08 14:36:35 -0700

    collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function for conducting multiplication between two numbers
+ * @a: pass parameters
+ * @b: pass parameters
+ * Return: returns 0
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
