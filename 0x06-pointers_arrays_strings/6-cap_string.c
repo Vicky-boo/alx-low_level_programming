6a7641564cd912431dce552067e44441cbfbe423
vicron <vicron@example.com>
2022-07-14 14:37:29 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
-* cap_string -capitalizes all words of a string
-* @str: the string to be capitalized
-* Return: prints the result
+* cap_string - capitalizes all words of a string
+* @s: input string.
+* Return: the pointer to dest.
 */
-char *cap_string(char *str)
+
+char *cap_string(char *s)
 {
-int a = 0;
-while (str[a])
+int count = 0, i;
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
 {
-while (!(str[a] >= 'a' && str[a] <= 'z'))
-a++;
-if (str[a - 1] == ' ' 
-str[a - 1] == '\t' 
-str[a - 1] == '\n' 
-str[a - 1] == ',' 
-str[a - 1] == ';' 
-str[a - 1] == '.' 
-str[a - 1] == '!' 
-str[a - 1] == '?' 
-str[a - 1] == '"' 
-str[a - 1] == '(' 
-str[a - 1] == ')' 
-str[a - 1] == '{' 
-str[a - 1] == '}' ||
-a == 0)
-str[a] -= 32;
-a++;
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == sep_words[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
 }
-return (str);
+}
+count++;
+}
+return (s);
 }
